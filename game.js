4559d473f3b3763f5081030041f54bfa8e86d918
Woosterwebdev <136399860+Woosterwebdev@users.noreply.github.com>
2023-11-08 18:50:18 -0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,40 @@
+let buttonColors = ["red", "blue", "green", "yellow"];
+
+let gamePattern = [];
+
+let userClickedPattern = [];
+
+$("")
+
+$(".btn").click(function() {
+    let userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+
+    animatePress(userChosenColor);
+});
+
+
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+    playSound(randomChosenColor);
+}
+
+function playSound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+
+    setTimeout(function() {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
